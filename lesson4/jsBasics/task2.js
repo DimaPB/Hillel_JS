d73af4a278312fcdcea82ea35a6d4dcac1603552
DimaPB <dima.shvets.work@gmail.com>
2025-02-04 01:33:06 +0200

    Lesson 4

---
+++ lesson4/jsBasics/task2.js
@@ -0,0 +1,9 @@
+// Завдання 2: Конкатенація радків та шаблонний рядок 
+
+const nameOne = "Tolik"
+const nameTwo = "Paulo"
+const sayHello = "Greetings.It's been a long time since you've been street racing."
+
+console.log(nameOne, nameTwo)
+console.log(sayHello)
+
---
+++ lesson4/jsBasics/task4.js
@@ -0,0 +1,5 @@
+// Завдання 4: Обчислення площі та об'єму 
+// Завдання 4.1
+
+radius = 14
+console.log(`Task 4.1 unswer =`, Math.PI*radius**2)
