2fa5bf73f4ef3076f5c698faa937707c0d50b5fc
DimaPB <dima.shvets.work@gmail.com>
2025-02-17 18:49:06 +0200

    HW fix

--- lesson7/task1.js
+++ lesson7/task1.js
@@ -1,15 +1,17 @@
-const handleEven = () =>{
-    return console.log("Nimber is even")
+const handleEven = () => {
+    return console.log("Number is even")
 }
 
-const handleOdd = () => console.log("Nimber is odd")
+const handleOdd = () => console.log("Number is odd")
 
 
-let handleNum = (a, firs, second) => {
-    a % 2 ? firs(handleEven): second(handleOdd)
+let handleNum = (a, first, second) => {
+    a % 2 ? second(handleOdd) : first(handleEven)
+
+
 
 }
-handleNum(5, handleEven, handleOdd)
+handleNum(5.5, handleEven, handleOdd)
 handleNum(4, handleEven, handleOdd)
 
 
