fcf690a2f2c168faf54676c0b53b790973df43b8
DimaPB <dima.shvets.work@gmail.com>
2025-02-23 01:36:21 +0200

    fix

--- lesson8/task6.js
+++ lesson8/task6.js
@@ -1,4 +1,5 @@
 const numbersList = [1, 10, 14, 2, 4, 5, 43, 34]
 
-const sortedArray = numbersList.sort((a, b) => a - b)
+const sortedArray = numbersList.slice().sort((a, b) => a - b)
 console.log(sortedArray)
+// console.log(numbersList)
--- lesson9/task1.js
+++
@@ -1,7 +0,0 @@
-const student = {
-    firstName: "Shrek",
-    secondName: "Green",
-    age: 50,
-    courses: ["Math", "History", "Right History", "Music"]
-};
-console.log(student)
