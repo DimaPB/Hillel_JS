d0bc515771ff645e6bff1619ef0e7ceb9cfc5634
DimaPB <dima.shvets.work@gmail.com>
2025-02-23 10:48:56 +0200

    HW 9

---
+++ lesson9/task3.js
@@ -0,0 +1,15 @@
+const car1 = {
+    brand: "Nisan",
+    model: "Skyline",
+    year: 2004
+}
+
+const car2 = {
+    brand: "Doge",
+    model: "Challenger",
+    owner: "Piter"
+}
+
+const car3 = { ...car1, ...car2 }
+
+console.log(car3)
