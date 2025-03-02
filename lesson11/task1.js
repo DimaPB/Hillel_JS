8112d5745b4841c27aedffb7bdafd09344bd8576
DimaPB <dima.shvets.work@gmail.com>
2025-03-02 17:49:25 +0200

    HW 11

---
+++ lesson11/task1.js
@@ -0,0 +1,5 @@
+function simpleFunc(text, time) {
+    setTimeout(() => { console.log(text); }, time)
+}
+
+simpleFunc("new world", 4000)
---
+++ lesson11/task3.js
@@ -0,0 +1,29 @@
+async function callToServer() {
+    try {
+        const promiseOne = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        const data = await promiseOne.json()
+        // console.log(data)
+        return data
+    } catch (error) {
+
+    }
+}
+
+async function secondCallToServer() {
+    try {
+        const promiseTwo = await fetch('https://jsonplaceholder.typicode.com/users/1')
+        const data = await promiseTwo.json()
+        // console.log(data)
+        return data
+    } catch (error) {
+
+    }
+}
+
+
+const promiseOne = await Promise.all([callToServer(), secondCallToServer()])
+console.log(promiseOne)
+
+const promiseTwo = await Promise.race([callToServer(), secondCallToServer()])
+console.log(promiseTwo)
+
